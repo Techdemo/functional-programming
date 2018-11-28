c046a63e2d34763673cd68a2be295d08a9b9169f
Marcel <fleuren.marcel@gmail.com>
2018-11-28 11:59:55 +0100

    plotted dots

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 // specify the svg container
-const svg = d3
-  .select(".chart")
-  .append("svg")
-  .attr("width", 1000)
-  .attr("height", 1000);
 
 d3.json("dataset.json").then(data => {
-  console.log("Alle data:", data);
+  let bookNested = d3
+    .nest()
+    .key(data => data.Year)
+    .rollup(function(data) {
+      return data.length;
+    })
+    .entries(data);
 
-  let allBooks = data.length;
+  const svg = d3
+    .select(".chart")
+    .append("svg")
+    .attr("width", 1400)
+    .attr("height", 1400);
 
-  let bookYears = [...new Set(data.map(d => Number(d.Year)))];
-  console.log("Object alle jaren van de boeken", bookYears);
-  console.log("allbooks", allBooks);
-  console.log(Object.values(bookYears));
+  console.log(bookNested);
+  let yearCount = bookNested.map(d => {
+    return Number(d.key);
+  });
+  let bookCount = bookNested.map(d => {
+    return Number(d.value);
+  });
+
+  console.log("dit is Yearcount", yearCount);
+  console.log("dit is bookCount", bookCount);
+
+  let dataYears = data.map(d => {
+    return Number(d.Year);
+  });
 
   // set the domains and ranges for the axis
   let y = d3
     .scaleLinear()
-    .domain([data.length, 0])
+    .domain([d3.max(bookCount), d3.min(bookCount)])
     .range([0, 500]);
   let x = d3
     .scaleLinear()
-    .domain([d3.min(bookYears), d3.max(bookYears)])
+    .domain([d3.min(yearCount), d3.max(yearCount)])
     .range([0, 750]);
 
   // group the axis
@@ -36,19 +51,32 @@ d3.json("dataset.json").then(data => {
     .attr("color", "white");
 
   // create and call the axisLeft
-  let y_Axis = d3.axisLeft(y).ticks(6);
-  let x_Axis = d3.axisBottom(x).tickFormat(d3.format("d"));
+  let y_Axis = d3.axisLeft().scale(y);
+  let x_Axis = d3
+    .axisBottom()
+    .scale(x)
+    .tickFormat(d3.format("d"));
   xAxisGroup.call(x_Axis);
   yAxisGroup.call(y_Axis);
 
   // create circles for books by year
-  const circles = svg.selectAll("circle").data(data);
+  const circles = svg
+    .selectAll("circle")
+    .attr("cx", yearCount)
+    .attr("cy", bookCount)
+    .data(bookNested);
 
   circles
     .enter()
     .append("circle")
     .attr("r", 5)
-    .attr("cx", d)
-    .attr("cy", d => y)
-    .attr("fill", "white");
+    // .attr("cx", d => dataYears)
+    .attr("cx", d => {
+      return x(Number(d.key));
+    })
+    .attr("cy", d => {
+      return y(Number(d.value));
+    })
+    .attr("fill", "white")
+    .attr("transform", "translate(42,0)");
 });
